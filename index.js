614ac7cf3ff5ad99e496646b958db3785851d25e
DeveloperFahad <developerfahad.me@gmail.com>
2024-05-10 16:46:03 +0600

    Create api for categories from backend successfully.

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
+    const categoryCollection = client.db('libraryManagerDB').collection('categories');
+
+
+    app.get('/categories', async(req, res) =>{
+      const cursor = categoryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
